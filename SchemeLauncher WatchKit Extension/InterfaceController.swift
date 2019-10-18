be1fbc84a0f8157899b6b31c79d8bb4cf56790c5
山上 忍 <yamakami_shinobu_gn@cyberagent.co.jp>
2019-10-18 20:17:23 +0900

    first

---
+++ SchemeLauncher WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  InterfaceController.swift
+//  SchemeLauncher WatchKit Extension
+//
+//  Created by 山上 忍 on 2019/10/15.
+//  Copyright © 2019 SL. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
